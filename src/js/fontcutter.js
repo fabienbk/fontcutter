ad7d0f780736bccb17dbb3e3715a6a9ab8952890
Fabien Benoit-Koch <fabien.benoit@gmail.com>
2014-05-10 15:33:21 +0200

    early work

--- src/js/fontcutter.js
+++ src/js/fontcutter.js
@@ -1,41 +1,46 @@
-function CanvasManager(canvasId) {
-	var me = this;
-	me.canvas = document.getElementById(canvasId);
-	me.ctx = me.canvas.getContext('2d');
+function CanvasManager(canvasId, canvasContainerId) {
+	this.canvas = document.getElementById(canvasId);
+	this.ctx = this.canvas.getContext('2d');
+    this.canvasContainer = document.getElementById(canvasContainerId);
+    this.lineTable = new LineTable($('#linenumber').val(), $('#lineTable'));
 
 	var imageLoader = document.getElementById('imageLoader');
-	imageLoader.addEventListener('change', me.handleImage, false);
+	imageLoader.addEventListener('change', this.handleImage.bind(this), false);
 
 	var lineNumberField = document.getElementById('linenumber');
-	lineNumberField.addEventListener('change', me.onLineNumberChange, false);
-
-	me.handleImage = function(e) {
-		var reader = new FileReader();
-
-		reader.onload = function(event) {
-			var img = new Image();
-
-			img.onload = function() {
-				me.canvas.width = img.width;
-				me.canvas.height = img.height;
-				me.ctx.drawImage(img, 0, 0);
-			};
-
-			img.src = event.target.result;
-		};
-
-		reader.readAsDataURL(e.target.files[0]);
-	};
-
-	me.onLineNumberChange = function(event) {
-		var newLineNumber = $('#linenumber').val();
-		if (newLineNumber > lineTable.lineNumber) {
-			lineTable.addLines(newLineNumber - lineTable.lineNumber);
-		} else if (newLineNumber < lineTable.lineNumber) {
-			lineTable.removeLines(lineTable.lineNumber - newLineNumber);
-		}
-	};
+	lineNumberField.addEventListener('change', this.onLineNumberChange.bind(this), false);
 }
+
+
+CanvasManager.prototype.handleImage = function(e) {
+        var reader = new FileReader();
+        var canvasContainerId = this.canvasContainerId;
+        var me = this;
+
+        reader.onload = function(event) {
+            var img = new Image();
+
+            img.onload = function() {
+                me.canvas.width = img.width;
+                me.canvas.height = img.height;
+                me.ctx.drawImage(img, 0, 0);
+            };
+
+            img.src = event.target.result;
+        };
+
+        reader.readAsDataURL(e.target.files[0]);
+    };
+
+CanvasManager.prototype.onLineNumberChange = function(event) {
+        var newLineNumber = $('#linenumber').val();
+        alert(newLineNumber + " and table ");
+        if (newLineNumber > this.lineTable.lineArray.length) {
+            this.lineTable.addLines(newLineNumber - this.lineTable.lineArray.length);
+        } else if (newLineNumber < this.lineTable.lineArray.length) {
+            this.lineTable.removeLines(this.lineTable.lineArray.length - newLineNumber);
+        }
+    };
 
 /**
  * Line table
@@ -46,29 +51,29 @@ function LineDefinition(index, definition) {
 }
 
 LineDefinition.prototype.toHTML = function() {
-	return "<tr><td>" + this.index + "</td><td></td></tr>";
+	return "<tr><td>" + this.index + "</td><td><input type='text' name='glyphLine' value='ABCDEF'/></td></tr>";
 };
 
-function LineTable(lineNumber) {
-	this.lineNumber = lineNumber;
+function LineTable(lineNumber, jqueryObj) {
 	this.lineArray = [];
-
+    this.jqueryObj = jqueryObj;
 	this.addLines(lineNumber);
 }
 
 LineTable.prototype.addLines = function(number) {
+    alert("add " + number + "lines");
 	for (var i = 0; i < number; i++) {
-		var newIndex = this.lineNumber.length;
+		var newIndex = this.lineArray.length;
+        this.lineArray[newIndex] = lineDef;
 		var lineDef = new LineDefinition(newIndex, "");
-		this.lineNumber[newIndex] = lineDef;
-
-		//	$('#lineTable').append(newIndex.toHTML());
-
+		
+		this.jqueryObj.append(lineDef.toHTML());
 	}
 };
 
 LineTable.prototype.removeLines = function(number) {
-	this.lineNumber.length = number;
-};
+        alert("remove to " + number + "lines");
+	this.lineArray.length = number;
 
-var lineTable = new LineTable($('#linenumber').val());
+    // remove lines in jquery
+};
