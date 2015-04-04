8c70584993f60b2f8355f9433f00b224417e5a93
chalgoman <chalgoman@gmail.com>
2015-04-04 15:06:23 +0300

    Added tabs in XML output

--- js/fontcutter.js
+++ js/fontcutter.js
@@ -178,6 +178,7 @@ function  generateOutput() {
 
 function generateXMLOutput($scope) {
   var EOL = "\n";
+  var TAB = "\t";
   var output = '<?xml version="1.0"?>'+EOL;
 
   //padding for each character (up, right, down, left).
@@ -189,12 +190,12 @@ function generateXMLOutput($scope) {
   };
 
   output += '<font>'+EOL;
-  output += '<info face="'+fileName.name+'" size="'+$scope.charWidth+'" bold="0" italic="0" charset="" unicode="1" stretchH="100" smooth="1" aa="1" padding="'+padding+'" spacing="1,1" outline="0" />'+EOL;
-  output += '<common lineHeight="32" base="25" scaleW="'+image.width+'" scaleH="'+image.height+'" pages="1" packed="0" alphaChnl="1" redChnl="0" greenChnl="0" blueChnl="0" />'+EOL;
-  output += '<pages>'+EOL;
-  output += '<page id="0" file="'+fileName.name+'" />'+EOL;
-  output += '</pages>'+EOL;
-  output += '<chars count="'+count+'">'+EOL;
+  output += TAB + '<info face="'+fileName.name+'" size="'+$scope.charWidth+'" bold="0" italic="0" charset="" unicode="1" stretchH="100" smooth="1" aa="1" padding="'+padding+'" spacing="1,1" outline="0" />'+EOL;
+  output += TAB + '<common lineHeight="32" base="25" scaleW="'+image.width+'" scaleH="'+image.height+'" pages="1" packed="0" alphaChnl="1" redChnl="0" greenChnl="0" blueChnl="0" />'+EOL;
+  output += TAB + '<pages>'+EOL;
+  output += TAB + TAB + '<page id="0" file="'+fileName.name+'" />'+EOL;
+  output += TAB + '</pages>'+EOL;
+  output += TAB + '<chars count="'+count+'">'+EOL;
 
   for(var line=0; line < $scope.lineData.length; line++) {
     for (var i = 0; i < $scope.lineData[line].glyphs.length; i++) {
@@ -203,11 +204,11 @@ function generateXMLOutput($scope) {
       var height = $scope.charHeight;
       var x = i*width;
       var y = line*height;      
-      output += '<char id="'+character.charCodeAt(0)+'"   x="'+x+'"    y="'+y+'"     width="'+width+'"    height="'+height+'" xoffset="0"     yoffset="0"     xadvance="'+width+'"    page="0"  chnl="0" />'+EOL;
+      output += TAB + TAB + '<char id="'+character.charCodeAt(0)+'"   x="'+x+'"    y="'+y+'"     width="'+width+'"    height="'+height+'" xoffset="0"     yoffset="0"     xadvance="'+width+'"    page="0"  chnl="0" />'+EOL;
     }   
   }
 
-  output += '</chars>'+EOL;
+  output += TAB + '</chars>'+EOL;
   output += '</font>';
 
   $('#output').val(output);
